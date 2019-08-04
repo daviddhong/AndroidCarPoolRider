30312dab0afa651cc7a9fae7109c6f45e71ac6fa
david hong <davidhong022@gmail.com>
2019-08-03 22:49:28 -0700

    -m log in page and make account page made. without firebase

---
+++ app/src/main/java/android/carpoolrider/StartFromLogIn/LogInActivity.java
@@ -0,0 +1,109 @@
+package android.carpoolrider.StartFromLogIn;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.carpoolrider.MainActivity;
+import android.carpoolrider.R;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.Nullable;
+
+public class LogInActivity extends AppCompatActivity {
+    private Button loginButton;
+    private TextView MakeAccountPage;
+    private EditText loginEmail, loginPassword;
+//    private FirebaseAuth mAuth;
+//    private FirebaseUser currentUser;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        initializeFields();
+        logInWithEmailAndPassword();
+        goToMakeAccountPage();
+    }
+
+    private void initializeFields() {
+//        mAuth = FirebaseAuth.getInstance();
+//        currentUser = mAuth.getCurrentUser();
+        MakeAccountPage = findViewById(R.id.textView_makeaccount);
+        loginButton = findViewById(R.id.button_login);
+        loginEmail = findViewById(R.id.editText_email_login);
+        loginPassword = findViewById(R.id.editText_password_login);
+    }
+
+    // Make account to use to log in
+    private void goToMakeAccountPage() {
+        MakeAccountPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SendToMakeAccountActivity();
+            }
+        });
+    }
+
+    private void SendToMakeAccountActivity() {
+        Intent intent = new Intent(LogInActivity.this, MakeAccountActivity.class);
+        startActivity(intent);
+        // EFFECTS: Animation from LogInActivity to MakeAccountActivity.
+        LogInActivity.this.overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+    }
+
+
+    // Log In when login button is pressed
+    private void logInWithEmailAndPassword() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get strings from email and password editText fields
+                String email = loginEmail.getText().toString();
+                String password = loginPassword.getText().toString();
+                if (email.isEmpty()) {
+                    Toast.makeText(LogInActivity.this, "Please enter Your Email", Toast.LENGTH_LONG).show();
+                } else if (password.isEmpty()) {
+                    Toast.makeText(LogInActivity.this, "Please enter Your Password", Toast.LENGTH_LONG).show();
+                } else {
+//                    mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//                        @Override
+//                        public void onComplete(@NonNull Task<AuthResult> task) {
+//                            if (task.isSuccessful()) {
+                    SendToMainActivity();
+//                            } else {
+//                                Toast.makeText(LogInActivity.this, task.getException().toString(), Toast.LENGTH_LONG).show();
+//                            }
+//                        }
+//                    });
+                }
+            }
+        });
+    }
+
+
+    // go to main activity
+    private void SendToMainActivity() {
+        Intent intent = new Intent(LogInActivity.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        // EFFECTS: Animation from LogInActivity to MainActivity.
+        LogInActivity.this.overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        finish();
+    }
+
+    //checks if user is already logged in when app is started
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        AutomaticLoginFunctionIfUserIsAlreadySignedIn();
+    }
+    private void AutomaticLoginFunctionIfUserIsAlreadySignedIn() {
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        if (currentUser != null) {
+//            SendToMainActivity();
+    }
+}
---
+++ app/src/main/java/android/carpoolrider/StartFromLogIn/MakeAccountActivity.java
@@ -0,0 +1,99 @@
+package android.carpoolrider.StartFromLogIn;
+
+import android.carpoolrider.R;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MakeAccountActivity extends AppCompatActivity {
+
+    private Button confirmAccountInformationButton;
+    private EditText makeEmail, makePassword, firstName, lastName;
+//    private FirebaseAuth mAuth;
+//    private DatabaseReference RootRef;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_account);
+        initializeFields();
+        confirmAccountInformation();
+    }
+
+    // initialize the fields at the beginning
+    private void initializeFields() {
+//        mAuth = FirebaseAuth.getInstance();
+//        RootRef = FirebaseDatabase.getInstance().getReference();
+        makeEmail = (EditText) findViewById(R.id.editText_email_make);
+        makePassword = (EditText) findViewById(R.id.editText_pw_make);
+        firstName = (EditText) findViewById(R.id.editText_firstname_make);
+        lastName = (EditText) findViewById(R.id.editText_lastname_make);
+    }
+
+    //make account with name email and password. Then goes to verify phone number page
+    private void confirmAccountInformation() {
+        confirmAccountInformationButton = findViewById(R.id.button_confirm_make);
+        confirmAccountInformationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = makeEmail.getText().toString();
+                String password = makePassword.getText().toString();
+                String firstN = firstName.getText().toString();
+                String lastN = lastName.getText().toString();
+                if (firstN.isEmpty()) {
+                    Toast.makeText(MakeAccountActivity.this, "Please Enter Your First Name", Toast.LENGTH_LONG).show();
+                } else if (lastN.isEmpty()) {
+                    Toast.makeText(MakeAccountActivity.this, "Please Enter Your Last Name", Toast.LENGTH_LONG).show();
+                } else if (email.isEmpty()) {
+                    Toast.makeText(MakeAccountActivity.this, "Please Enter Your Email", Toast.LENGTH_LONG).show();
+                } else if (password.isEmpty()) {
+                    Toast.makeText(MakeAccountActivity.this, "Please Enter Your Password", Toast.LENGTH_LONG).show();
+                } else {
+//                    mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//                        @Override
+//                        public void onComplete(@NonNull Task<AuthResult> task) {
+//                            if (task.isSuccessful()) {
+//                                CollectFirstLastNameIntoRealTimeDatabase(firstN, lastN);
+                    SendVerificationEmail();
+//
+//                            } else {
+//                                // if account is not made
+//                                Toast.makeText(MakeAccountActivity.this, task.getException().toString(), Toast.LENGTH_LONG).show();
+//                            }
+//                        }
+//                    });
+
+                }
+            }
+        });
+    }
+
+    // add first last name to realtime database
+    private void CollectFirstLastNameIntoRealTimeDatabase(String firstN, String lastN) {
+//        String currentUserID = mAuth.getCurrentUser().getUid();
+//        HashMap<String, String> profileMap = new HashMap<>();
+//        profileMap.put("uid", currentUserID);
+//        profileMap.put("firstname", firstN);
+//        profileMap.put("lastname", lastN);
+//        RootRef.child("Users").child(currentUserID).setValue(profileMap);
+    }
+
+    // send to phone number activity
+    private void SendVerificationEmail() {
+        Intent intent = new Intent(MakeAccountActivity.this, LogInActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        // EFFECTS: Animation from MakeAccountActivity to PhoneNumberActivity.
+        MakeAccountActivity.this.overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+    }
+
+}
+
+
