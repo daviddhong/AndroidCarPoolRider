b824182dcb37f9958903887c41e80839421e80cf
david hong <davidhong022@gmail.com>
2019-08-13 20:27:55 -0700

    -m show all current users rider requests from firebase realtime database recycler view

--- app/src/main/java/android/carpoolrider/RequestRides/RequestRidesFragment.java
+++ app/src/main/java/android/carpoolrider/RequestRides/RequestRidesFragment.java
@@ -2,40 +2,44 @@ package android.carpoolrider.RequestRides;
 
 import android.carpoolrider.Profile.ProfileActivity;
 import android.carpoolrider.R;
-import android.carpoolrider.RequestRides.RequestNewRide.AndroidArchitectureRequestNewRide.TicketAdapter;
-import android.carpoolrider.RequestRides.RequestNewRide.AndroidArchitectureRequestNewRide.TicketEntity;
 import android.carpoolrider.RequestRides.RequestNewRide.AndroidArchitectureRequestNewRide.TicketViewModel;
-import android.carpoolrider.RequestRides.RequestNewRide.Later.Confirm.LaterConfirmActivity;
 import android.carpoolrider.RequestRides.RequestNewRide.RequestNewRideActivity;
+import android.carpoolrider.RequestRides.RequestNewRide.RiderRequestTicket;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebStorage;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.List;
-
-import static android.app.Activity.RESULT_OK;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class RequestRidesFragment extends Fragment {
 
-    View requestRidesView;
-    ImageView profileImageView;
-    RelativeLayout requestNewRideRelativeLayout;
-
+    private View requestRidesView;
+    private ImageView profileImageView;
+    private RelativeLayout requestNewRideRelativeLayout;
     private TicketViewModel ticketViewModel;
+
+    private RecyclerView FriendRecyclerView;
+    private DatabaseReference RiderTicketsRef, UsersRef;
+    private FirebaseAuth mAuth;
+    private String currentUserID;
 
     public static final int ADD_TICKET_REQUEST = 1;
 
@@ -44,21 +48,7 @@ public class RequestRidesFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         requestRidesView = inflater.inflate(R.layout.fragment_request_rides, container, false);
 
-        RecyclerView recyclerView = (RecyclerView)  requestRidesView.findViewById(R.id.rides_requested_recycler_view);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        recyclerView.setHasFixedSize(true);
-
-        final TicketAdapter ticketAdapter = new TicketAdapter();
-        recyclerView.setAdapter(ticketAdapter);
-
-        ticketViewModel = ViewModelProviders.of(this).get(TicketViewModel.class); // **** try getActivity() if this doesn't work
-        ticketViewModel.getAllTicketEntities().observe(this, new Observer<List<TicketEntity>>() {
-
-            @Override
-            public void onChanged(List<TicketEntity> ticketEntities) {
-                ticketAdapter.setTicketEntities(ticketEntities);
-            }
-        });
+        displaysFriendsListbyRecyclerView();
 
         // EFFECTS: Call setProfileImageView.
         setProfileImageView();
@@ -68,6 +58,86 @@ public class RequestRidesFragment extends Fragment {
 
         return requestRidesView;
     }
+
+    //use recycler view and friend list adapter to display list of friends
+    private void displaysFriendsListbyRecyclerView() {
+        FriendRecyclerView = (RecyclerView) requestRidesView.findViewById(R.id.rides_requested_recycler_view);
+        FriendRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+        RiderTicketsRef = FirebaseDatabase.getInstance().getReference().child("RiderTickets").child(currentUserID);
+        UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+    }
+
+
+    // create firebase recycler
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions options = new FirebaseRecyclerOptions.Builder<RiderRequestTicket>()
+                .setQuery(RiderTicketsRef, RiderRequestTicket.class)
+                .build();
+
+        final FirebaseRecyclerAdapter<RiderRequestTicket, riderTicketHolder> adapter
+                = new FirebaseRecyclerAdapter<RiderRequestTicket, riderTicketHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull riderTicketHolder riderticketholder, int i, @NonNull RiderRequestTicket riderReqTickets) {
+                String usersIDS = getRef(i).getKey();
+                UsersRef.child(usersIDS).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        if (dataSnapshot.exists()) {
+                            final String riderTo = dataSnapshot.child("To").getValue().toString();
+                            final String riderFrom = dataSnapshot.child("From").getValue().toString();
+                            riderticketholder.riderTo.setText(riderTo);
+                            riderticketholder.riderFrom.setText(riderFrom);
+                            riderticketholder.itemView.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    // does nothing when clicked yet (probably want to delete it)
+                                }
+                            });
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                    }
+                });
+            }
+
+            @NonNull
+            @Override
+            public riderTicketHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_request_ride_ticket_entity, parent, false);
+                riderTicketHolder viewHolder = new riderTicketHolder(view);
+                return viewHolder;
+            }
+        };
+        FriendRecyclerView.setAdapter(adapter);
+        adapter.startListening();
+    }
+
+
+
+    public static class riderTicketHolder extends RecyclerView.ViewHolder {
+        TextView riderTo, riderFrom;
+        public riderTicketHolder(@NonNull View itemView) {
+            super(itemView);
+            riderFrom = itemView.findViewById(R.id.text_origin);
+            riderTo = itemView.findViewById(R.id.text_destination);
+        }
+    }
+
+
+
+
+
+
+
 
     // EFFECTS: Set OnClickActivity for ProfileActivity.
     private void setProfileImageView() {
